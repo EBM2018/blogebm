c8be62e9e9e8337613d387cf1ffd8810213dc3a5
Lenophie <quasiphies@gmail.com>
2018-12-04 14:32:35 +0100

    Use compacted articles in "Home" view

--- database/seeds/ArticlesSeeder.php
+++ database/seeds/ArticlesSeeder.php
@@ -15,7 +15,18 @@ class ArticlesSeeder extends Seeder
     {
         Schema::disableForeignKeyConstraints();
         Article::create([
-            'title' => 'Bonjour !',
+            'title' => 'Théorie du complot : les chatons ont envahi le monde',
+            'summary' => 'De plus en plus nombreux dans les familles mais aussi dans la rue, les chatons s\'organisent pour dominer le monde',
+            'author_id' => 1
+        ]);
+        Article::create([
+            'title' => 'Vianney se questionne',
+            'summary' => 'Mais t\'es où Hamza ?',
+            'author_id' => 1
+        ]);
+        Article::create([
+            'title' => 'Pikachu et Bakura sont de retour',
+            'summary' => 'Dans la classe EBM, les émulateurs font rage. Yu-Gi-Oh et Pokémon sont au coeur des esprits. Facebook a du souci à se faire...',
             'author_id' => 1
         ]);
         Schema::enableForeignKeyConstraints();
