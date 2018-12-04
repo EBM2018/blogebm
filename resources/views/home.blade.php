5d6482061efed7add7970b5eb1709d06849d6272
Lenophie <quasiphies@gmail.com>
2018-12-04 13:50:59 +0100

    Create "Home" controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,14 +12,14 @@
 @include ('header')
 <div class="container is-fluid">
     <div class="columns">
-        <div class="column ">
-                <h4 class = "title is-4"> Mes articles</h4>
+        <div class="column">
+                <h4 class="title is-4"> Mes articles</h4>
         </div>
     </div>
     <div class="columns">
-        <div class="column ">
+        <div class="column">
             <div class="box">
-                <h6 class = "title is-6"> Théorie du complot : les chatons ont envahi le monde</h6>
+                <h6 class="title is-6"> Théorie du complot : les chatons ont envahi le monde</h6>
                 <p>De plus en plus nombreux dans les familles mais aussi dans la rue, les chatons s'organisent pour dominer le monde</p>
             </div>
         </div>
