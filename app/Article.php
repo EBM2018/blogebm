b53d82240a18c02f3e3b0c94a2ac7a18bf3b26f5
Lenophie <quasiphies@gmail.com>
2018-12-04 14:25:05 +0100

    Compact articles in "Home" view

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    public static function allSummarized() {
+        $articles = Article::select('id', 'title', 'summary');
+        return $articles;
+    }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $articles = Article::allSummarized()->get();
+        return view('home', compact('articles'));
     }
 }
