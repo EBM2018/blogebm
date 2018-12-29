037ee9a47bb9e14122798b86dad0f09cccd0fdc6
Lenophie <quasiphies@gmail.com>
2018-12-29 18:53:14 +0100

    Add relation between "Article" and "Paragraph" models

--- app/Article.php
+++ app/Article.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    /**
+     * Get the article's paragraphs.
+     */
+    public function paragraphs() {
+        return $this->hasMany('App\Paragraph')->orderBy('order');
+    }
+
     public static function allSummarized() {
         $articles = Article::select('id', 'title', 'summary');
         return $articles;
