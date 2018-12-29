ddec91c7e0ff41a8cca135aa65e6915bd6f21213
Lenophie <quasiphies@gmail.com>
2018-12-29 18:58:44 +0100

    Add link to "Home" page in header

--- resources/views/header.php
+++ resources/views/header.php
@@ -1,7 +1,9 @@
 <div class="container is-fluid">
     <header class="columns">
         <div class="column is-4">
-            <h1 class="title">Blogemb !</h1>
+            <h1 class="title">
+                <a id="header-home-link" href="/">Blogemb !</a>
+            </h1>
         </div>
         <div class="column is-4 is-offset-4">
             <button class="button is-link is-pulled-right">Se connecter</button>
