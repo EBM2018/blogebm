fcbbfe6dd096341f2b7a7a359204c1d7ba62ecee
Lenophie <quasiphies@gmail.com>
2018-12-30 21:19:03 +0100

    Add basic layout to article creation view

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -13,7 +13,30 @@
     <div class="container is-fluid">
         <div class="columns">
             <div class="column">
-                Ready for a new article !
+                <form>
+                    <div class="field">
+                        <label class="label" for="title">{{ __("blogebm.title") }}</label>
+                        <div class="control">
+                            <input id="title" name="title" class="input" placeholder=""/>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label" for="summary">{{ __("blogebm.summary") }}</label>
+                        <div class="control">
+                            <textarea id="summary" name="summary" class="textarea" placeholder=""></textarea>
+                        </div>
+                    </div>
+                    <label class="label">{{ __("blogebm.content") }}</label>
+                    <a class="button is-primary" id="add-paragraph-button" type="submit">{{ __("blogebm.add_paragraph") }}</a>
+                    <hr>
+                    <div class="field">
+                        <div class="control">
+                            <a type="submit" class="button is-link">
+                                {{ __('Confirm') }}
+                            </a>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
