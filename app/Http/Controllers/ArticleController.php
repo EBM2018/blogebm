7591d37e69750ad20844dc799a33dfaceaea45f8
Lenophie <quasiphies@gmail.com>
2018-12-31 15:54:29 +0100

    Add article update route logic

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -58,18 +58,54 @@ class ArticleController extends Controller
 
     public function update(UpdateArticleRequest $request)
     {
-        // One of the operations is not CRUD (change order)
+        // One of the operations is not CRUD (change order) and manipulates multiple paragraphs
         // so I went with an article PATCH instead of manipulating a paragraph resource with 3 different operations.
         switch(request('type')) {
             case "CREATE_PARAGRAPH":
                 // Receive an article id with the new paragraph's content
                 // Answer with the new paragraph's id
-                break;
+
+                $newParagraph = null;
+                // Pass a reference to get the new paragraph id outside of the database transaction closure
+                DB::transaction(function() use(&$newParagraph) {
+                    // Get the position of the new paragraph
+                    $currentArticleParagraphs = Paragraph::where('article_id', request('article_id'))->get();
+                    $newLastPosition = $currentArticleParagraphs->max('order') + 1;
+
+                    // Create the new paragraph
+                    $newParagraph = Paragraph::create([
+                        'content' => request('content'),
+                        'article_id' => request('article_id'),
+                        'order' => $newLastPosition
+                    ]);
+
+                    // Touch the article to update its "last updated" date
+                    Article::where('id', request('article_id'))->first()->touch();
+                });
+
+                // Send the new paragraph's id
+                return response()->json([
+                    'paragraph_id' => $newParagraph->id
+                ]);
             case "CHANGE_PARAGRAPH_CONTENT":
                 // Receive a paragraph id with the new content
+                DB::transaction(function() {
+                    Paragraph::find(request('paragraph_id'))
+                        ->update([
+                            'content', request('content')
+                        ]);
+                });
                 break;
             case "CHANGE_PARAGRAPHS_ORDER":
                 // Receive the complete list of paragraphs ids with their new position
+                DB::transaction(function() {
+                   foreach (request('paragraphs') as $paragraph) {
+                       Paragraph::find(request($paragraph->id))
+                           ->update([
+                               'order' => $paragraph->order
+                           ]);
+                   }
+                });
                 break;
             default:
                 // Type of the PATCH request is not part of the established enumeration
