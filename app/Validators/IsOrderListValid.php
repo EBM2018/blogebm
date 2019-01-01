50db6d5603af6104c436786bd8058f7f9af30a15
Lenophie <quasiphies@gmail.com>
2019-01-01 22:29:48 +0100

    Validate order lists

--- app/Validators/IsOrderListValid.php
+++ app/Validators/IsOrderListValid.php
@@ -6,6 +6,21 @@ class IsOrderListValid
 {
     public function validate($attribute, $value, $parameters, $validator)
     {
+        $listLength = count($value);
+
+        // Check if order values are not out of bounds
+        foreach ($value as $paragraph) {
+            if ($paragraph["order"] < 1 || $paragraph["order"] > $listLength) return false;
+        }
+
+        // Check if order values are unique
+        for ($i = 1; $i <= $listLength; $i++) {
+            foreach ($value as $paragraph) {
+                if ($paragraph["order"] === $i) continue 2;
+            }
+            return false;
+        }
+
         return true;
     }
 }
