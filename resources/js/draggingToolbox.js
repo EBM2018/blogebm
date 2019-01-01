1a80997a46988f45f15b7517831500d6e4437572
Lenophie <quasiphies@gmail.com>
2019-01-01 19:51:57 +0100

    Add commentary to dragging toolbox

--- resources/js/draggingToolbox.js
+++ resources/js/draggingToolbox.js
@@ -1,25 +1,48 @@
 import {getByClass, getById, remove} from "./toolbox.js";
 
+/**
+ * Handles the start of a drag
+ * @param {Event} event
+ * @param {string} draggableClassName
+ * @param {Element} parentNode
+ */
 export const dragstartHandler = (event, draggableClassName, parentNode) => {
     event.dataTransfer.setData("text/plain", event.target.id);
     addDropZones(draggableClassName, parentNode);
 };
 
-export const dragendHandler = (event) => {
+/**
+ * Handles the end of a drag
+ */
+export const dragendHandler = () => {
     removeDropzones();
 };
 
+/**
+ * Handles the drag motion
+ * @param {Event} event
+ */
 const dragoverHandler = (event) => {
     event.preventDefault();
     event.dataTransfer.dropEffect = "move"
 };
 
+/**
+ * Handles a drop
+ * @param {Event} event
+ * @param {Element} parentNode
+ */
 const dropHandler = (event, parentNode) => {
     event.preventDefault();
     const data = event.dataTransfer.getData("text/plain");
     parentNode.replaceChild(getById(data), event.target);
 };
 
+/**
+ * Adds drop zones for dragged element
+ * @param {string} draggableClassName
+ * @param {Element} parentNode
+ */
 const addDropZones = (draggableClassName, parentNode) => {
     let i = 0;
     for (const childNode of parentNode.getElementsByClassName(draggableClassName)) {
@@ -33,11 +56,19 @@ const addDropZones = (draggableClassName, parentNode) => {
     parentNode.appendChild(lastDropZone);
 };
 
+/**
+ * Removes dropzones
+ */
 const removeDropzones = () => {
     const dropzones = getByClass('dropzone');
     remove(dropzones);
 };
 
+/**
+ * Creates a dropzone
+ * @param {Element} parentNode
+ * @returns {Element}
+ */
 const createDropZone = (parentNode) => {
     const dropZone = document.createElement('div');
     dropZone.classList.add('dropzone');
